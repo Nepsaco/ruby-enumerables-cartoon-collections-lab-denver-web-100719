03228bf335ccf014370aa67312c7706eb17aed68
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-23 17:21:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ end
 
 def summon_captain_planet(planet_calls)
   planet_calls.map{|a|
-      
+    "#{a.capitalize}!"    
   }
+  planet_calls
 end
 
 def long_planeteer_calls# code an argument here
