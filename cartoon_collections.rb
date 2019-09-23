7311a2104da0d6190441b9cc2b074a60a4053191
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-23 17:06:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.to_s
+  dwarfs.each_with_index {|dwarfs,index|
+    dwarfs[index] = index
+  }
+  dwarfs
 end
 
 def summon_captain_planet# code an argument here
